863316760170861ca36e840af2fbc26e8b19f4a6
Hamza Zaheer <62650512+Hamzazaheer721@users.noreply.github.com>
2022-10-04 19:16:02 +0500

    feat: REACT-TS BOILERPLATE UPDATED

---
+++ .eslintrc.js
@@ -0,0 +1,59 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    jest: true, //
+  },
+  root: true,
+  extends: [
+    "react-app",
+    "react-app/jest",
+    "airbnb",
+    "airbnb-typescript",
+    "plugin:import/typescript",
+    // 'plugin:prettier/recommneded', // deprecated
+    "airbnb/hooks", //
+    "plugin:@typescript-eslint/recommended", //
+    "plugin:@typescript-eslint/recommended-requiring-type-checking", //
+  ],
+  overrides: [],
+  // parser: '@typescript-eslint/parser', // deprecated
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+    project: "./tsconfig.json",
+    ecmaFeatures: {
+      //
+      jsx: true,
+    },
+  },
+  plugins: ["react", "@typescript-eslint", "prettier"],
+  rules: {
+    quotes: [
+      "error",
+      "double",
+      {
+        avoidEscape: true,
+      },
+    ],
+    "@typescript-eslint/quotes": [
+      "error",
+      "double",
+      {
+        avoidEscape: true,
+      },
+    ],
+    "react/react-in-jsx-scope": "off",
+    "react/jsx-filename-extension": [
+      1,
+      {
+        extensions: [".js", ".jsx", ".ts", ".tsx"],
+      },
+    ],
+    "jsx-quotes": ["error", "prefer-double"], // double quotes in jsx dont throw error
+    "react/jsx-props-no-spreading": ["warn"],
+    "react/jsx-uses-react": ["off"],
+    "no-shadow": "off",
+    "no-underscore-dangle": "off",
+  },
+};
