7f225c156a1dc4e17075b881739b150346c485e3
Hamza Zaheer <62650512+Hamzazaheer721@users.noreply.github.com>
2022-10-05 19:16:30 +0500

    feat/routes: fixed the route error in consoles

--- src/general/types/index.ts
+++ src/general/types/index.ts
@@ -3,6 +3,7 @@ import { FunctionComponent, ReactNode } from "react";
 export type RoutesType = {
   path: string;
   Component: FunctionComponent;
+  key: string;
 };
 export interface IErrorBoundryState {
   hasError: boolean;
